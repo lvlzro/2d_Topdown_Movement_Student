12ba5f64ac826cb77aa27baa71bd5dfb039f861a
Laurence S <33729843+lvlzro@users.noreply.github.com>
2020-03-26 12:54:45 -0300

    simple 2d controller

---
+++ Assets/CameraMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public GameObject player;
+    public Camera cam;
+    Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = cam.transform.position - player.transform.position;
+    }
+    private void LateUpdate()
+    {
+        cam.transform.position = player.transform.position + offset;
+    }
+}
